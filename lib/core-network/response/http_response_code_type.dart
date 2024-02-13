8c7e9fe6a6d1c2250a7c42b2eeef64218894f2fd
Denis Latushko <denis.latushko92@gmail.com>
2024-02-13 16:18:21 +0300

    Added code types

--- lib/core-network/response/http_response_code_type.dart
+++ lib/core-network/response/http_response_code_type.dart
@@ -1,7 +1,9 @@
 ///A named types for all the HTTP response codes with its values
 enum HttpResponseCodeType {
   unknown(httpCode: -1),
-  success(httpCode: 200);
+  success(httpCode: 200),
+  notFound(httpCode: 404),
+  serverError(httpCode: 500);
 
   const HttpResponseCodeType({required this.httpCode});
 
